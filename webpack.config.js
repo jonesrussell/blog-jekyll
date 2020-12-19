cb111c4aa8192526f980b64fa0e61b612b1171c0
Russell Jones <jonesrussell42@gmail.com>
2020-12-19 19:06:50 +0000

    split up webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
-const path = require('path');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const svgToMiniDataURI = require('mini-svg-data-uri');
+const path = require('path')
+const glob = require('glob')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const PurgeCSSPlugin = require('purgecss-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const svgToMiniDataURI = require('mini-svg-data-uri')
+
+const PATHS = {
+  src: path.join(__dirname, 'assets')
+}
 
 module.exports = {
   entry: './src/index',
   output: {
     path: path.resolve(__dirname, 'assets/js/'),
-    filename: 'app.bundle.js'
+    filename: '[name].bundle.js'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
   },
   module: {
     rules: [
@@ -17,17 +36,22 @@ module.exports = {
         // Include ts, tsx, js, and jsx files.
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          'style-loader',
+          'css-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       },
       {
         test: /\.(woff|woff2|ttf|eot)$/,
         use: {
-          loader: 'url-loader',
-        },
+          loader: 'url-loader'
+        }
       },
       {
         test: /\.svg$/i,
@@ -35,12 +59,20 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              generator: (content) => svgToMiniDataURI(content.toString()),
-            },
-          },
-        ],
-      },
-    ],
+              generator: content => svgToMiniDataURI(content.toString())
+            }
+          }
+        ]
+      }
+    ]
   },
-  plugins: [new ForkTsCheckerWebpackPlugin()]
-};
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true })
+    })
+  ]
+}
