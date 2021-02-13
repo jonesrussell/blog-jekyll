d71ce0e465626101837317120381429b3f3dec11
Russell Jones <jonesrussell42@gmail.com>
2021-02-13 15:31:39 +0000

    add iso date to pubsub

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,23 +1,17 @@
 const NRP = require('node-redis-pubsub');
 const url = process.env.REDIS_URL;
 
-console.log('url', url);
-
 const config = {
   url: url
 };
 
-console.log('config', config);
-
 const nrp = new NRP(config); // This is the NRP client
 
-const foo = {
+const postDetails = {
   url: 'https://blog.jonesrussell42.xyz/feed.json',
-  hello: 'world'
+  date: new Date().toISOString
 };
 
-console.log('foo', foo);
-
-nrp.emit('blog:post', foo);
+nrp.emit('blog:post', postDetails);
 
 nrp.quit();
