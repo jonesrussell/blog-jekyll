b092125a60c54cabeeddba1c5df68ff1502c1584
Russell Jones <jonesrussell42@gmail.com>
2021-02-13 06:03:19 +0000

    WIP

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,11 +1,13 @@
 const NRP = require('node-redis-pubsub');
 const url = process.env.REDIS_URL;
 
-console.log(url);
+console.log('url', url);
 
 const config = {
   url: url
 };
+
+console.log('config', config);
 
 const nrp = new NRP(config); // This is the NRP client
 
