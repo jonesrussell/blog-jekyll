9bb6ce8dd45505ea8bac431096faf78fe8fef48d
Russell Jones <jonesrussell42@gmail.com>
2021-02-13 05:24:15 +0000

    pubsub

---
+++ src/pubsub.js
@@ -0,0 +1,12 @@
+const NRP = require('node-redis-pubsub');
+const url = process.env.REDIS_URL;
+
+const config = {
+  url: url
+};
+
+const nrp = new NRP(config); // This is the NRP client
+
+nrp.emit('blog:post', { url: 'https://blog.jonesrussell42.xyz/feed.json' });
+
+nrp.quit();
